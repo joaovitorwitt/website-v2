421cdc4e78b6b0394ea9a0790a996ec4625664d3
João Vitor Witt <joaovitorwitt2004@gmail.com>
2023-10-24 13:12:42 -0300

    Articles Fix
    articles are now sorted by most recent

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -23,34 +23,36 @@ export default function Articles() {
       <Header />
       <div className="container">
         <div className="posts-wrapper">
-          {ArticleList.map((article, index) => (
-            <Link
-              key={index}
-              className="article"
-              to={`/articles/${article.id}`}
-            >
-              <div className="article-wrapper">
-                <div className="posts-article-image-wrapper">
-                  <img
-                    src={article.image}
-                    key={article.id}
-                    alt=""
-                    className="article-image"
-                  />
-                </div>
-
-                <div className="article-data-container">
-                  <div className="article-data">
-                    <span>{article.date}</span>
-                    <span className="article-data-spacer"></span>
+          {ArticleList.slice() // Create a shallow copy of the array
+            .sort((a, b) => b.id - a.id) // Sort in descending order by id
+            .map((article, index) => (
+              <Link
+                key={index}
+                className="article"
+                to={`/articles/${article.id}`}
+              >
+                <div className="article-wrapper">
+                  <div className="posts-article-image-wrapper">
+                    <img
+                      src={article.image}
+                      key={article.id}
+                      alt=""
+                      className="article-image"
+                    />
                   </div>
 
-                  <h3 className="article-title">{article.title}</h3>
-                  <p className="article-description">{article.description}</p>
+                  <div className="article-data-container">
+                    <div className="article-data">
+                      <span>{article.date}</span>
+                      <span className="article-data-spacer"></span>
+                    </div>
+
+                    <h3 className="article-title">{article.title}</h3>
+                    <p className="article-description">{article.description}</p>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
         </div>
       </div>
     </div>
